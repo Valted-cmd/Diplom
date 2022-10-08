1af1e2896b3b16923fa425440e33b790a759eb2f
Adavletbaev <alex.davletbaev@yandex.ru>
2022-10-08 16:15:02 +0300

    init 1

---
+++ src/main/java/ru/netology/diplom/HelloController.java
@@ -0,0 +1,14 @@
+package ru.netology.diplom;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
