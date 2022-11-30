9cad70a3804f2b228aa7cb3060c460a7154ae121
Adavletbaev <alex.davletbaev@yandex.ru>
2022-11-30 23:33:58 +0300

    edited files

--- src/test/java/ru/netology/page/Page.java
+++ src/test/java/ru/netology/page/Page.java
@@ -2,6 +2,7 @@ package ru.netology.page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
